a8ded65cdc36191b7c92ea5cb263e6753ee9f724
Joanderson Felipe Silva Barbosa <joandersonfsbarbosa@gmail..com>
2023-10-13 16:55:25 -0300

    in process

---
+++ todolist/src/main/java/br/com/vixetech/todolist/task/TaskModel.java
@@ -0,0 +1,30 @@
+package br.com.vixetech.todolist.task;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity(name = "tb_tasks")
+public class TaskModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    private UUID idUser;
+}
